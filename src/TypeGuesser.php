256bad3f2c54ee1b5233680431f055ab7029e2ae
Martin Kirilov <wucdbm@gmail.com>
2018-08-14 01:09:24 +0300

    fix typo in copyright

--- src/TypeGuesser.php
+++ src/TypeGuesser.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the ForciStaticDataBundle package.
+ * This file is part of the wucdbm/credit-card-guesser package.
  *
  * Copyright (c) Martin Kirilov <martin@forci.com>
  *
